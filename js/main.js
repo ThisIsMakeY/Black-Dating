49e2ea3d465a1ef2dc5d278d6460bc39050c8d2e
ThisIsMakeY <85873829+ThisIsMakeY@users.noreply.github.com>
2021-08-01 19:35:26 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+    $('.header__burger,.header__nav-link').click(function(event) {
+        $('.header__burger,.header__nav').toggleClass('active');
+    });
+
+
+});
